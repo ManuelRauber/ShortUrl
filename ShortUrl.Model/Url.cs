4be0aea19098975be642b69e460ab9ff9422e28e
Manuel Rauber <github@manuel-rauber.de>
2013-08-10 17:46:31 +0200

    added basic models

---
+++ ShortUrl.Model/Url.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortUrl.Model
+{
+	public class Url
+	{
+		public string Id { get; set; }
+		public string LongUrl { get; set; }
+		public DateTime? ExpirationDate { get; set; }
+		public int HitCount { get; set; }
+		public virtual User User { get; set; }
+	}
+}
