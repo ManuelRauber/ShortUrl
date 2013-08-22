ef5ee91c50b68c6375cabe97ab603c6b4a65222b
Manuel Rauber <github@manuel-rauber.de>
2013-08-22 19:10:11 +0200

    moved bootstrap and webapiconfig to common folder

--- ShortUrl/Common/Bootstrap.cs
+++ ShortUrl/Common/Bootstrap.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web;
+﻿using System.Reflection;
 using System.Web.Http;
-using System.Web.UI;
 using Autofac;
 using Autofac.Integration.WebApi;
 
-namespace ShortUrl
+namespace ShortUrl.Common
 {
 	public static class Bootstrap
 	{
--- ShortUrl/Global.asax.cs
+++ ShortUrl/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using ShortUrl.Common;
 using ShortUrl.DataAccessLayer;
 
 namespace ShortUrl
