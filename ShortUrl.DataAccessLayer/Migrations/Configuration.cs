f380abe8deebd956b0b743dbd4dd41b7d1f7bcfb
Manuel Rauber <github@manuel-rauber.de>
2013-10-03 10:48:28 +0200

    removed seeding admin user method and corresponding static variables

--- ShortUrl.DataAccessLayer/Migrations/Configuration.cs
+++ ShortUrl.DataAccessLayer/Migrations/Configuration.cs
@@ -11,28 +11,9 @@ namespace ShortUrl.DataAccessLayer.Migrations
 
 	internal sealed class Configuration : DbMigrationsConfiguration<ShortUrl.DataAccessLayer.ShortUrlContext>
 	{
-		private static readonly string _adminUserName = "Admin";
-		private static readonly string _adminRoleName = "Administrator";
-
 		public Configuration()
 		{
 			AutomaticMigrationsEnabled = true;
-		}
-
-		protected override void Seed(ShortUrl.DataAccessLayer.ShortUrlContext context)
-		{
-			bool tmp;
-
-			if (Boolean.TryParse(ConfigurationManager.AppSettings["CreateAdminUser"], out tmp)
-				&& (tmp))
-			{
-				SeedAdminUser(context);
-			}
-		}
-
-		private void SeedAdminUser(ShortUrlContext context)
-		{
-
 		}
 	}
 }
