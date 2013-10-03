3002572eb3fd9c739fc326d7f2e7111a98d4d33a
Manuel Rauber <github@manuel-rauber.de>
2013-10-03 16:17:19 +0200

    added a textmediatype formatter

---
+++ ShortUrl.Core/Formatters/TextMediaTypeFormatter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortUrl.Core.Formatters
+{
+	public class TextMediaTypeFormatter : MediaTypeFormatter
+	{
+		public TextMediaTypeFormatter()
+		{
+			SupportedEncodings.Add(Encoding.UTF8);
+			SupportedMediaTypes.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
+		}
+
+		public override Task WriteToStreamAsync(Type type, object value, Stream writeStream, HttpContent content,
+			TransportContext transportContext)
+		{
+			return Task.Factory.StartNew(() =>
+			{
+
+				var str = (string)value;
+
+				using (var textWriter = new StreamWriter(writeStream))
+				{
+					textWriter.Write(str);
+				}
+			});
+		}
+
+		public override bool CanReadType(Type type)
+		{
+			return false;
+		}
+
+		public override bool CanWriteType(Type type)
+		{
+			return typeof(string) == type;
+		}
+	}
+}
