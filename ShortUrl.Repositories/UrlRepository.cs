07b7a3437185e9f4b83596f2f5f18083c8411f5d
Manuel Rauber <github@manuel-rauber.de>
2013-10-03 13:56:12 +0200

    using the simple pattern generator in urlrepository

--- ShortUrl.Repositories/UrlRepository.cs
+++ ShortUrl.Repositories/UrlRepository.cs
@@ -1,4 +1,6 @@
-﻿using ShortUrl.DataAccessLayer;
+﻿using System.Linq;
+using ShortUrl.Core.Patterns;
+using ShortUrl.DataAccessLayer;
 using ShortUrl.DataAccessLayer.Contacts;
 using ShortUrl.Model;
 using ShortUrl.Repositories.Contracts;
@@ -7,7 +9,31 @@ namespace ShortUrl.Repositories
 {
 	public class UrlRepository : Repository<Url>, IUrlRepository
 	{
-		public UrlRepository(IDatabaseFactory factory) : base(factory)
-		{ }
+		private readonly IPatternGenerator _patternGenerator;
+
+		public UrlRepository(IDatabaseFactory factory, IPatternGenerator patternGenerator) : base(factory)
+		{
+			_patternGenerator = patternGenerator;
+		}
+
+		public override Url Add(Url entity)
+		{
+			string key;
+
+			do
+			{
+				key = _patternGenerator.Generate();
+			}
+			while (KeyExists(key));
+
+			entity.Id = key;
+
+			return base.Add(entity);
+		}
+
+		private bool KeyExists(string key)
+		{
+			return Get().Any(x => x.Id.Equals(key));
+		}
 	}
 }
