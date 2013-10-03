248fb5230ed0495c9c836ee75e188127f2a7af6f
Manuel Rauber <github@manuel-rauber.de>
2013-10-03 13:58:09 +0200

    registering the types to the autofac container

--- ShortUrl/Common/Bootstrap.cs
+++ ShortUrl/Common/Bootstrap.cs
@@ -2,8 +2,13 @@
 using System.Web.Http;
 using Autofac;
 using Autofac.Integration.WebApi;
+using ShortUrl.Core.Patterns;
 using ShortUrl.DataAccessLayer;
 using ShortUrl.DataAccessLayer.Contacts;
+using ShortUrl.Repositories;
+using ShortUrl.Repositories.Contracts;
+using ShortUrl.Services;
+using ShortUrl.Services.Contracts;
 
 namespace ShortUrl.Common
 {
@@ -22,6 +27,9 @@ namespace ShortUrl.Common
 
 			builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
 			builder.RegisterType<DatabaseFactory>().As<IDatabaseFactory>();
+			builder.RegisterType<UrlRepository>().As<IUrlRepository>();
+			builder.RegisterType<SimplePatternGenerator>().As<IPatternGenerator>();
+			builder.RegisterType<UrlService>().As<IUrlService>();
 
 			var container = builder.Build();
 
