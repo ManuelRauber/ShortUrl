9e89a25e52d9dc64655d8138ae7f87430d818826
Manuel Rauber <github@manuel-rauber.de>
2013-10-29 22:42:40 +0100

    added database initializer for automatic migrations

--- ShortUrl.DataAccessLayer/DatabaseFactory.cs
+++ ShortUrl.DataAccessLayer/DatabaseFactory.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ShortUrl.DataAccessLayer.Contacts;
+using ShortUrl.DataAccessLayer.Migrations;
 
 namespace ShortUrl.DataAccessLayer
 {
 	public class DatabaseFactory : IDatabaseFactory
 	{
 		private ShortUrlContext _shortUrlContext;
+
+		// TODO: Maybe refactor this?
+		static DatabaseFactory()
+		{
+			(new DatabaseInitializer<ShortUrlContext, Configuration>()).Initialize();
+		}
 
 		public ShortUrlContext GetShortUrlContext()
 		{
---
+++ ShortUrl.DataAccessLayer/DatabaseInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Threading;
+
+namespace ShortUrl.DataAccessLayer
+{
+	// TODO: Does this make sense?
+	public class DatabaseInitializer<TContext, TConfiguration>
+		where TContext: DbContext
+		where TConfiguration: DbMigrationsConfiguration<TContext>, new()
+	{
+		private class AutomaticDatabaseMigration<TMigrationContext, TMigrationConfiguration>
+			where TMigrationContext : TContext
+			where TMigrationConfiguration : DbMigrationsConfiguration<TMigrationContext>, new()
+		{
+			public AutomaticDatabaseMigration()
+			{
+				
+				Database.SetInitializer(new MigrateDatabaseToLatestVersion<TMigrationContext, TMigrationConfiguration>());
+			}
+		}
+
+		private object _lock;
+		private AutomaticDatabaseMigration<TContext, TConfiguration> _initializer;
+		private bool _isInitialized;
+
+		public void Initialize()
+		{
+			LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _lock);
+		}
+	}
+}
