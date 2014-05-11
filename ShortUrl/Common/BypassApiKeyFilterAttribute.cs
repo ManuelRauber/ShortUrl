050d4e811ea73b43ad7389df0264626e16b84452
Manuel Rauber <github@manuel-rauber.de>
2014-05-11 18:33:35 +0200

    updated year in license file

---
+++ ShortUrl/Common/BypassApiKeyFilterAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Autofac.Integration.WebApi;
+
+namespace ShortUrl.Common
+{
+	// Don't do anything here :)
+	public class BypassApiKeyFilterAttribute : IAutofacActionFilter
+	{
+		public void OnActionExecuting(HttpActionContext actionContext)
+		{}
+
+		public void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+		{}
+	}
+}
